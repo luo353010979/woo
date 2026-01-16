c41de270ea77f642647c6c3cf768eb45acfea8fe
Luo Hao <353010979@qq.com>
2026-01-17 02:45:39 +0800

    add -> 4.1 ScreenUtil 配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:woo/common/index.dart';
@@ -20,20 +21,38 @@ class MyApp extends StatelessWidget {
       dark: AppTheme.dark,
       initial: ConfigService.to.themeMode,
       debugShowFloatingThemeButton: true,
-      builder: (theme, darkTheme) => GetMaterialApp(
-        title: 'Flutter Demo',
-        theme: theme,
-        darkTheme: darkTheme,
-        // initialRoute: RouteNames.systemSplash,
-        initialRoute: RouteNames.stylesStyleIndex,
-        getPages: RoutePages.list,
-        navigatorObservers: [RoutePages.observers],
-        // 多语言
-        translations: Translation(), // 词典
-        localizationsDelegates: Translation.localizationsDelegates, // 代理
-        supportedLocales: Translation.supportedLocales, // 支持的语言种类
-        locale: ConfigService.to.locale, // 当前语言种类
-        fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+      builder: (theme, darkTheme) => ScreenUtilInit(
+        designSize: const Size(414, 896),
+        splitScreenMode: false, // 支持分屏尺寸
+        minTextAdapt: false,    // 是否根据宽度/高度中的最小值来适配文字
+        builder: (context, child) => GetMaterialApp(
+          title: 'Flutter Demo',
+          theme: theme,
+          darkTheme: darkTheme,
+          // initialRoute: RouteNames.systemSplash,
+          initialRoute: RouteNames.stylesStyleIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observers],
+          // 多语言
+          translations: Translation(), // 词典
+          localizationsDelegates: Translation.localizationsDelegates, // 代理
+          supportedLocales: Translation.supportedLocales, // 支持的语言种类
+          locale: ConfigService.to.locale, // 当前语言种类
+          fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+
+           // builder
+          builder: (context, widget) {
+            // 不随系统字体缩放比例
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: widget!,
+            );
+          },
+
+          debugShowCheckedModeBanner: false,
+
+        ),
       ),
     );
   }
