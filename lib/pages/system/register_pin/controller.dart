c3fbc15a227092488861b84b19adbe3b2f606346
luohao <353010979@qq.com>
2026-01-18 23:15:28 +0800

    add -> 6.5 Pin 界面编写

--- lib/pages/system/register_pin/controller.dart
+++ lib/pages/system/register_pin/controller.dart
@@ -1,7 +1,38 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:woo/common/index.dart';
 
 class RegisterPinController extends GetxController {
   RegisterPinController();
+
+  // ping 文字输入控制器
+  TextEditingController pinController = TextEditingController();
+
+  // 表单 key
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  // 这里默认一个 pin 值，生产环境在服务端验证
+  String pinCheckValue = '111111';
+
+  // 验证 pin
+  String? pinValidator(val) {
+    return val == pinCheckValue
+        ? null
+        : LocaleKeys.commonMessageIncorrect.trParams({"method": "Pin"});
+  }
+
+  // pin 触发提交
+  void onPinSubmit(String val) {
+    debugPrint("onPinSubmit: $val");
+  }
+
+  // 按钮提交
+  void onBtnSubmit() {}
+
+  // 按钮返回
+  void onBtnBackup() {
+    Get.back();
+  }
 
   _initData() {
     update(["register_pin"]);
@@ -20,8 +51,9 @@ class RegisterPinController extends GetxController {
     _initData();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  @override
+  void onClose() {
+    super.onClose();
+    pinController.dispose();
+  }
 }
