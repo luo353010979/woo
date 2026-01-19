60ff758903ffee3f1295b055f67204c1e83da54b
luohao <353010979@qq.com>
2026-01-20 04:27:53 +0800

    add -> 8.1 导航栏编写

--- lib/common/components/index.dart
+++ lib/common/components/index.dart
@@ -4,3 +4,4 @@ export 'welcome_slider.dart';
 export 'slider_indicator.dart';
 export 'page_title.dart';
 export 'pin.dart';
+export 'navigation.dart';
--- lib/pages/system/main/view.dart
+++ lib/pages/system/main/view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:woo/common/index.dart';
 
 import 'index.dart';
 
@@ -26,8 +28,72 @@ class _MainViewGetX extends GetView<MainController> {
   const _MainViewGetX();
 
   // 主视图
-  Widget _buildView() {
-    return const Center(child: Text("MainPage"));
+  Widget _buildView(BuildContext context) {
+    return PopScope(
+      // 允许返回
+      canPop: false,
+
+      // 防止连续点击两次退出
+      onPopInvokedWithResult: (bool didPop, dynamic result) {
+        // 如果返回，则不执行退出请求
+        if (didPop) {
+          return;
+        }
+
+        // 退出请求
+        if (controller.closeOnConfirm(context)) {
+          SystemNavigator.pop(); // 系统级别导航栈 退出程序
+        }
+      },
+
+      child: Scaffold(
+        extendBody: true,
+        resizeToAvoidBottomInset: false,
+        // 导航栏
+        bottomNavigationBar: GetBuilder<MainController>(
+          id: 'navigation',
+          builder: (controller) {
+            return BuildNavigation(
+              currentIndex: controller.currentIndex,
+              items: [
+                NavigationItemModel(
+                  label: LocaleKeys.tabBarHome.tr,
+                  icon: AssetsSvgs.navHomeSvg,
+                ),
+                NavigationItemModel(
+                  label: LocaleKeys.tabBarCart.tr,
+                  icon: AssetsSvgs.navCartSvg,
+                  count: 3,
+                ),
+                NavigationItemModel(
+                  label: LocaleKeys.tabBarMessage.tr,
+                  icon: AssetsSvgs.navMessageSvg,
+                  count: 9,
+                ),
+                NavigationItemModel(
+                  label: LocaleKeys.tabBarProfile.tr,
+                  icon: AssetsSvgs.navProfileSvg,
+                ),
+              ],
+              onTap: controller.onJumpToPage, // 切换tab事件
+            );
+          },
+        ),
+        // 内容页
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: controller.pageController,
+          onPageChanged: controller.onIndexChanged,
+          children: const [
+            // 加入空页面占位
+            Text("1"),
+            Text("2"),
+            Text("3"),
+            Text("4"),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
@@ -38,7 +104,7 @@ class _MainViewGetX extends GetView<MainController> {
       builder: (_) {
         return Scaffold(
           appBar: AppBar(title: const Text("main")),
-          body: SafeArea(child: _buildView()),
+          body: SafeArea(child: _buildView(context)),
         );
       },
     );
