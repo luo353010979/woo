0cd459f38b71274dbd17714306520bd7ee06c881
luohao <353010979@qq.com>
2026-01-20 02:32:55 +0800

    add -> 7.1 登录界面编写

--- lib/pages/system/login/controller.dart
+++ lib/pages/system/login/controller.dart
@@ -1,7 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:woo/common/index.dart';
 
 class LoginController extends GetxController {
   LoginController();
+
+  /// 表单 key
+  GlobalKey formKey = GlobalKey<FormState>();
+
+  /// 用户名
+  TextEditingController userNameController = TextEditingController(
+    text: "ducafecat5",
+  );
+
+  /// 密码
+  TextEditingController passwordController = TextEditingController(
+    text: "123456",
+  );
+
+  /// Sign In 登入
+  Future<void> onSignIn() async {
+    if ((formKey.currentState as FormState).validate()) {
+      try {
+        Loading.show();
+
+        Get.back(result: true);
+      } finally {
+        Loading.dismiss();
+      }
+    }
+  }
+
+  /// Sign Up 注册
+  void onSignUp() {
+    Get.offNamed(RouteNames.systemRegister);
+  }
 
   _initData() {
     update(["login"]);
@@ -20,8 +53,11 @@ class LoginController extends GetxController {
     _initData();
   }
 
-  // @override
-  // void onClose() {
-  //   super.onClose();
-  // }
+  /// 释放
+  @override
+  void onClose() {
+    super.onClose();
+    userNameController.dispose();
+    passwordController.dispose();
+  }
 }
